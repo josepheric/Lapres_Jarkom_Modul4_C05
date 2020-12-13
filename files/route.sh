16ab3c8c5e9815ae17ee9051daf444a930f775eb
FeinardSlim <58687783+FeinardSlim@users.noreply.github.com>
2020-12-13 17:25:29 +0700

    Create route.sh

---
+++ files/route.sh
@@ -0,0 +1,57 @@
+#Surabaya-00
+route add -net 192.168.64.0 netmask 255.255.252.0 gw 192.168.64.2
+route add -net 192.168.68.0 netmask 255.255.255.252 gw 192.168.68.2
+route add -net 192.168.16.0 netmask 255.255.255.252 gw 192.168.68.2
+route add -net 192.168.8.0 netmask 255.255.255.128 gw 192.168.68.2
+route add -net 192.168.0.0 netmask 255.255.248.0 gw 192.168.68.2
+route add -net 192.168.32.0 netmask 255.255.252.0 gw 192.168.68.2
+route add -net 192.168.136.0 netmask 255.255.255.252 gw 192.168.136.2
+route add -net 192.168.128.0 netmask 255.255.254.0 gw 192.168.136.2
+route add -net 192.168.130.0 netmask 255.255.255.240 gw 192.168.136.2
+route add -net 192.168.132.0 netmask 255.255.252.0 gw 192.168.136.2
+route add -net 192.168.152.0 netmask 255.255.255.252 gw 192.168.136.2
+route add -net 192.168.148.0 netmask 255.255.255.0 gw 192.168.136.2
+route add -net 192.168.144.0 netmask 255.255.252.0 gw 192.168.136.2
+route add -net 10.151.77.48 netmask 255.255.255.252 gw 192.168.136.2
+route add -net 10.151.77.52 netmask 255.255.255.252 gw 10.151.77.54
+
+#batu-00
+route add -net 0.0.0.0 netmask 0.0.0.0 gw 192.168.136.1
+route add -net 192.168.136.0 netmask 255.255.255.252 gw 192.168.136.1
+route add -net 192.168.128.0 netmask 255.255.254.0 gw 192.168.128.2
+route add -net 192.168.128.0 netmask 255.255.254.0 gw 192.168.128.3
+route add -net 192.168.130.0 netmask 255.255.255.240 gw 192.168.128.2
+route add -net 192.168.132.0 netmask 255.255.252.0 gw 192.168.132.2
+route add -net 192.168.152.0 netmask 255.255.255.252 gw 192.168.152.2
+route add -net 192.168.148.0 netmask 255.255.255.0 gw 192.168.152.2
+route add -net 192.168.144.0 netmask 255.255.252.0 gw 192.168.152.2
+route add -net 10.151.77.48 netmask 255.255.255.252 gw 192.168.152.2
+
+#Kediri-00
+route add -net 0.0.0.0 netmask 0.0.0.0 gw 192.168.152.1
+route add -net 192.168.152.0 netmask 255.255.255.252 gw 192.168.152.1
+route add -net 192.168.144.0 netmask 255.255.255.0 gw 192.168.148.2
+route add -net 192.168.148.0 netmask 255.255.252.0 gw 192.168.148.2
+route add -net 192.168.148.0 netmask 255.255.252.0 gw 192.168.148.3
+route add -net 10.151.77.48 netmask 255.255.255.252 gw 10.151.77.50
+
+#Blitar-00
+route add -net 0.0.0.0 netmask 0.0.0.0 gw 192.168.148.1
+route add -net 192.168.144.0 netmask 255.255.252.0 gw 192.168.144.2
+
+#Madiun-00
+route add -net 0.0.0.0 netmask 0.0.0.0 gw 192.168.128.1
+route add -net 192.168.130.0 netmask 255.255.255.240 gw 192.168.130.2
+
+#Pasuruan-00
+route add -net 0.0.0.0 netmask 0.0.0.0 gw 192.168.68.1
+route add -net 192.168.68.0 netmask 255.255.255.252 gw 192.168.68.1
+route add -net 192.168.16.0 netmask 255.255.255.252 gw 192.168.16.2
+route add -net 192.168.8.0 netmask 255.255.255.128 gw 192.168.16.2
+route add -net 192.168.0.0 netmask 255.255.248.0 gw 192.168.16.2
+route add -net 192.168.32.0 netmask 255.255.252.0 gw 192.168.32.2
+
+#Probolingo-00
+route add -net 0.0.0.0 netmask 0.0.0.0 gw 192.168.16.1
+route add -net 192.168.8.0 netmask 255.255.255.128 gw 192.168.8.2
+route add -net 192.168.0.0 netmask 255.255.248.0 gw 192.168.0.2
